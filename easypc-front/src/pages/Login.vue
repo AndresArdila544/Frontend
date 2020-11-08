38466c3cb9ed02698b42471fd52808b627835ad5
VirtualDiegox <dbayonac@unal.edu.co>
2020-11-08 01:14:57 -0500

    No rgb login, in progress

--- easypc-front/src/pages/Login.vue
+++ easypc-front/src/pages/Login.vue
@@ -1,146 +1,123 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 col-sm-10 col-md-8 mx-auto">
-        <div class="mt-5">
+  <div class="row">
+    <v-card color="#837B1B" class="col col-lg-6 offset-lg-3 col-12">
+      <div class="col-12 mx-auto">
+        <div>
           <form class="form-inline" @submit="login">
-
-            <h2 class="col-12 text-center mt-3 mb-5 titulo-login">Iniciar Sesi&oacute;n</h2>
-
-            <div class="col-12 form-group ">
-              <label for="username" class="col-12 col-md-4 campos-login">Nombre de Usuario</label>
-              <div class="webflow-style-input col col-sm-10 col-md-8 offset-sm-1 offset-md-2">
-                  <input id="username" class="col-12 inputs-login" type="text"
-                                      placeholder="Nombre" v-model="username" required/>
-              </div>
-              
-            </div>
+            <v-title class="col-12 text-center mb-5 titulo-login">Iniciar Sesi&oacute;n</v-title>
 
             <div class="col-12 form-group">
-              <label for="password" class="col-12 col-md-4 campos-login">Contrase&ntilde;a</label>
-              
-              
-              <div class="webflow-style-input col col-sm-10 col-md-8 offset-sm-1 offset-md-2">
-                <input id="password" class="inputs-login col-12" type="password"
-                     placeholder="Contraseña" v-model="password" required/>
+              <label for="username" class="col col-md-6 col-12 campos-login">Nombre de Usuario</label>
+              <div class="col col-sm-10 col-md-9 offset-md-2">
+                <v-text-field
+                  v-model="username"
+                  type="text"
+                  placeholder="NoobMaster69"
+                  :rules="[rules.required]"
+                  required
+                  append-icon=""
+                  color="rgb(59,22,100)"
+                ></v-text-field>
+              </div>
+            </div>
+            <div class="col-12 form-group">
+              <label for="password" class="col col-md-4 col-12 campos-login">Contrase&ntilde;a</label>
+              <div class="col col-sm-10 col-md-9 offset-md-2">
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show_pass ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required]"
+                  :type="show_pass ? 'text' : 'password'"
+                  color="rgb(59,22,100)"
+                  @click:append="show_pass = !show_pass"
+                ></v-text-field>
               </div>
             </div>
 
-            <div class="col-12 col-sm-12 col-md-5 text-center mb-3">
-                <span class="">
-                  <small>
-                    <router-link :to="{ name: 'SignUp' }">&iquest;No tienes una cuenta? &iexcl;Registrate!</router-link>
-                  </small>
-                </span>
+            <div class="col-12 col-sm-12 col-md-5 text-center mb-3 mx-auto">
+              <span class>
+                <small>
+                  <router-link
+                    :to="{ name: 'SignUp' }"
+                  >&iquest;No tienes una cuenta? &iexcl;Registrate!</router-link>
+                </small>
+              </span>
             </div>
-            <div class="col-12 col-sm-12 col-md-4 mb-3">
-              <vs-button 
-                class="col col-sm-10 col-md-10 offset-sm-1 offset-md-2 boton-login" 
+            <div class="col-12 col-sm-12 col-md-10 mb-3">
+              <vs-button
+                class="col col-sm-10 col-md-10 offset-sm-1 offset-md-2 boton-login"
                 type="submit"
-                color="rgb(59,22,100)">
-                Iniciar Sesi&oacute;n
+                color="rgb(59,22,100)"
+              >
+                <h2>Iniciar Sesi&oacute;n</h2>
               </vs-button>
             </div>
-
           </form>
         </div>
       </div>
-    </div>
-    <div class="row">
-      
-    </div>
+    </v-card>
   </div>
 </template>
 <script>
-import EasyPCService from "../services/EasyPCService"
+import EasyPCService from "../services/EasyPCService";
 
-    export default {
-        name: "Login.vue",
-        components: {},
-        data(){
-            return {
-                username: '',
-                password: ''
-            }
-        },
-        methods: {
-            login( event ){
-               EasyPCService.authentication(this.username,this.password).
-               then( response => {
-                    if( response.status !== 200 ){
-                        alert( "Error en la autenticación" );
-                    }else{
-                        localStorage.setItem( 'token', response.data.access_token );
-                        //alert( "¡Autenticación Exitosa! El token se ha almacenado en el Local Storage" )
-                        this.$router.push("/Admin");
-                    }
-                } ).catch( error => {
-                    if( error.response.status === 400 ){
-                      alert( "Credenciales incorrectas" );
-                    }else{
-                      alert( "¡Parece que hubo un error de comunicación con el servidor!" );
-                    }
-                } );
+export default {
+  name: "Login.vue",
+  components: {},
+  data() {
+    return {
+      username: "",
+      password: "",
+      show_pass: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+      },
+    };
+  },
+  methods: {
+    login(event) {
+      EasyPCService.authentication(this.username, this.password)
+        .then((response) => {
+          if (response.status !== 200) {
+            alert("Error en la autenticación");
+          } else {
+            localStorage.setItem("token", response.data.access_token);
+            //alert( "¡Autenticación Exitosa! El token se ha almacenado en el Local Storage" )
+            this.$router.push("/Admin");
+          }
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            alert("Credenciales incorrectas");
+          } else {
+            alert("¡Parece que hubo un error de comunicación con el servidor!");
+          }
+        });
 
-                event.preventDefault();
-            }
-        }
-    }
+      event.preventDefault();
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss" >
-  
 .boton-login {
   text-align: center;
   font-size: calc(1.2rem + 0.2vw);
-  font-family: 'Poppins', sans-serif; 
+  font-family: "Poppins", sans-serif;
 }
 
 .titulo-login {
   text-align: center;
-  font-size: calc(1.7rem + 0.8vw);
-  font-family: 'Poppins', sans-serif;
+  font-size: calc(1.9rem + 1.8vw);
+  font-family: "Poppins", sans-serif;
 }
-.campos-login{
-font-size: calc(1rem + 0.5vw);
+.campos-login {
+  font-size: calc(1rem + 0.5vw);
+  font-family: "Poppins", sans-serif;
 }
 .inputs-login {
-font-size: calc(1rem + 0.5vw);
-background-color: inherit;
-
+  font-size: calc(1rem + 0.5vw);
+  background-color: red;
 }
-@keyframes gradient { 
-  0%{background-position:0 0}
-  100%{background-position:100% 0}
-}
-$input-background: inherit;
-
-
-.webflow-style-input {
-  width: 100%;
-  &:after {
-    content: "";
-    position: absolute;
-    left: 0px;
-    right: 0px;
-    bottom: 0px;
-    z-index: 999;
-    height: 7px;
-    border-bottom-left-radius: 2px;
-    border-bottom-right-radius: 2px;
-    background-position: 0% 0%;
-    background: linear-gradient(to right, #B294FF, #57E6E6, #FEFFB8, #57E6E6, #B294FF, #57E6E6);
-    background-size: 500% auto;
-    animation: gradient 3s linear infinite;
-  }
-}
-
-.webflow-style-input input {
-  flex-grow: 1;
-  font-size: 1.8rem;
-  line-height: 2.4rem;
-  vertical-align: middle;
- 
-}
-
 </style>
