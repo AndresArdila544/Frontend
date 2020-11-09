c3d0f8c6af1b36e3228eac8fee6f84953df26a51
VirtualDiegox <dbayonac@unal.edu.co>
2020-11-09 16:19:25 -0500

    responsive summary

--- easypc-front/src/components/Summary/ComponentView.vue
+++ easypc-front/src/components/Summary/ComponentView.vue
@@ -1,78 +1,114 @@
 <template>
-  <div class="box p-2 container">
-    <h3 class="titulo pt-2">{{tipo}}</h3>
-    <p class="modelo">{{Parte.model}}</p>
-
-    <vs-row vs-type="flex" class="col-container py-1">
-      <vs-col class="col" w="4">
-        <div class="container-fluid">
-          
-          <div class="py-1">
-            <img class="image" :src="Parte.linkPicture" alt />
+  <div>
+    <div class="card box block">
+      <div class="card-header" id="headingOne">
+        <div class="row">
+          <div
+            class="col-12 text-center"
+            type="button"
+            data-toggle="collapse"
+            :data-target="'#' + tipo"
+            aria-expanded="true"
+            :aria-controls="tipo"
+          >
+            <h3 class="titulo">{{tipo}}</h3>
           </div>
-          
         </div>
-      </vs-col>
-      <vs-col class="col" direction="column" w="4">
-        <vs-col w="12">
-          <div class="pb-3 d-flex justify-content-left" v-if="Parte.linkSL || Parte.priceSL">
-            <div class="product__view--more">
-              <a target="_blank" class="texto" :href="Parte.linkSL" v-if="Parte.linkSL">Link SpeedLogic</a>
-              <div class="texto" v-else>Tienda SpeedLogic</div>
-              <div class="view__more--block" v-if="Parte.linkSL"></div>
+        <div class="collapse" :id="tipo" aria-labelledby="headingOne">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-12 text-center">
+                <h2 class="modelo">{{Parte.model}}</h2>
+              </div>
+            </div>
+            <div class="row mx-auto">
+              <div class="col-sm-5 col-md-4 col-12">
+                <img class="img-fluid" :src="Parte.linkPicture" alt="" />
+              </div>
+              <div class="col-sm-4 col-md-4 col-6">
+                <div class="pb-3 col-12" v-if="Parte.linkSL || Parte.priceSL">
+                  <div class="product__view--more">
+                    <a
+                      target="_blank"
+                      class="texto"
+                      :href="Parte.linkSL"
+                      v-if="Parte.linkSL"
+                    >Link SpeedLogic</a>
+                    <div class="texto" v-else>Tienda SpeedLogic</div>
+                    <div class="view__more--block" v-if="Parte.linkSL"></div>
+                  </div>
+                </div>
+                <div class="pb-3 col-12" v-if="Parte.linkTauret || Parte.priceTauret">
+                  <div class="product__view--more">
+                    <a
+                      target="_blank"
+                      class="texto"
+                      :href="Parte.linkTauret"
+                      v-if="Parte.linkTauret"
+
+                    >
+                    <div >
+                      Link Tauret
+                    </div>
+                    <div class="d-none d-sm-block">
+                      Computadores
+                    </div>
+                     </a>
+                    <div class="texto" v-else>Tienda Tauret Computadores</div>
+                    <div class="view__more--block" v-if="Parte.linkTauret"></div>
+                  </div>
+                </div>
+                <div class="pb-3 col-12" v-if="Parte.linkCyP || Parte.priceCyP">
+                  <div class="product__view--more">
+                    <a
+                      target="_blank"
+                      class="texto"
+                      :href="Parte.linkCyP"
+                      v-if="Parte.linkCyP"
+                    >
+                    <div>
+                      Link Clones
+                    </div>
+                    <div class="d-none d-sm-block">
+                       y Perifericos
+                    </div>
+                     </a>
+                    <div class="texto" v-else>Tienda Clones y Perifericos</div>
+                    <div class="view__more--block" v-if="Parte.linkCyP"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col col-sm-12 col-md-12">
+                <div class="pb-3 col-12" v-if="Parte.linkSL || Parte.priceSL">
+                  <div
+                    class="texto"
+                    v-if="Parte.priceSL"
+                  >${{Parte.priceSL.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
+                  <div class="texto" v-else>No tiene precio actualmente</div>
+                </div>
+                <div
+                  class="pb-3 col-12"
+                  v-if="Parte.linkTauret || Parte.priceTauret"
+                >
+                  <div
+                    class="texto"
+                    v-if="Parte.priceTauret"
+                  >${{Parte.priceTauret.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
+                  <div class="texto" v-else>No tiene precio actualmente</div>
+                </div>
+                <div class="pb-3 col-12" v-if="Parte.linkCyP || Parte.priceCyP">
+                  <div
+                    class="texto"
+                    v-if="Parte.priceCyP"
+                  >${{Parte.priceCyP.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
+                  <div class="texto" v-else>No tiene precio actualmente</div>
+                </div>
+              </div>
             </div>
           </div>
-        </vs-col>
-        <vs-col w="12">
-          <div class="pb-3 d-flex justify-content-left" v-if="Parte.linkTauret || Parte.priceTauret">
-            <div class="product__view--more">
-              <a target="_blank" class="texto" :href="Parte.linkTauret" v-if="Parte.linkTauret">Link Tauret Computadores</a>
-              <div class="texto" v-else>Tienda Tauret Computadores</div>
-              <div class="view__more--block" v-if="Parte.linkTauret"></div>
-            </div>
-          </div>
-        </vs-col>
-        <vs-col w="12">
-          <div class="pb-3 d-flex justify-content-left" v-if="Parte.linkCyP || Parte.priceCyP">
-            
-            <div class="product__view--more">
-              <a target="_blank" class="texto" :href="Parte.linkCyP" v-if="Parte.linkCyP">Link Clones y Perifericos</a>
-              <div class="texto" v-else>Tienda Clones y Perifericos</div>
-              <div class="view__more--block" v-if="Parte.linkCyP"></div>
-            </div>
-          </div>
-        </vs-col>
-      </vs-col>
-      <vs-col class="col" direction="column" w="4">
-        <vs-col align="right" w="12">
-          <div class="pb-3 d-flex flex-row-reverse" v-if="Parte.linkSL || Parte.priceSL">
-            <div
-              class="texto"
-              v-if="Parte.priceSL"
-            >${{Parte.priceSL.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
-            <div class="texto" v-else>No tiene precio actualmente</div>
-          </div>
-        </vs-col>
-        <vs-col w="12">
-          <div class="pb-3 d-flex flex-row-reverse" v-if="Parte.linkTauret || Parte.priceTauret">
-            <div
-              class="texto"
-              v-if="Parte.priceTauret"
-            >${{Parte.priceTauret.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
-            <div class="texto" v-else>No tiene precio actualmente</div>
-          </div>
-        </vs-col>
-        <vs-col w="12">
-          <div class="pb-3 d-flex flex-row-reverse" v-if="Parte.linkCyP || Parte.priceCyP">
-            <div
-              class="texto"
-              v-if="Parte.priceCyP"
-            >${{Parte.priceCyP.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}} COP</div>
-            <div class="texto" v-else>No tiene precio actualmente</div>
-          </div>
-        </vs-col>
-      </vs-col>
-    </vs-row>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -104,25 +140,28 @@ export default {
 @import url(https://fonts.googleapis.com/css2?family=Maven+Pro:wght@700&display=swap);
 
 .texto {
-  font-size: 2vw;
+  font-size: calc(1rem + 0.5vw);
   text-align: left;
   z-index: 2;
 }
 
 .titulo {
-  font-size: 2.7vw;
+  text-align: center;
+  font-size: calc(1.9rem + 1.8vw);
+  font-family: "Poppins", sans-serif;
 }
 .modelo {
-  font-size: 2vw;
+  text-align: center;
+  font-size: calc(1.5rem + 0.5vw);
+  font-family: "Poppins", sans-serif;
 }
 
 a:hover {
   text-decoration: none;
-  color:inherit;
-
+  color: inherit;
 }
 a {
-  color:#FF9941;
+  color: #ff9941;
 }
 
 .col-container {
@@ -135,42 +174,21 @@ a {
 }
 .image {
   max-width: 100%;
-  width: 400px;
 }
 
 .box {
   border-style: none none solid none;
   border-color: #3b1664;
-  color: #FF9941;
+  border-width: 10px;
+  color: #ff9941;
   text-decoration-style: wavy;
   font-family: "Maven Pro", sans-serif;
   background-color: rgb(24, 23, 23);
-  
 }
 
 .product__view--more {
   width: 100%;
   display: flex;
   align-items: center;
-  
-  cursor: pointer;
-
-  &:hover {
-    .view__more--block {
-      width: 26vw;
-      height: 4.5vw;
-    }
-  }
-
-  .view__more--block {
-    width: 0;
-    height: 0;
-    position: absolute;
-    background: #3b1664;
-    z-index: 1;
-    transition: cubic-bezier(0.68, -0.55, 0.27, 01.55) 520ms;
-    border-radius: 0 30% 30% 0;
-  }
-  
 }
 </style>
--- easypc-front/src/pages/Summary.vue
+++ easypc-front/src/pages/Summary.vue
@@ -1,52 +1,52 @@
 <template>
   <div class>
-    <p id class="Summary pt-4">Resumen # {{ CurrentBuild.idBuild }}</p>
+    <p class="Summary pt-4">Resumen # {{ CurrentBuild.idBuild }}</p>
     <p class="user">Build para {{ user }}</p>
-    <div class="pt-2 parts row">
-      <div class="p-1 col-12 col-md-6" v-if="CurrentCPU">
+    <div class="pt-2 parts row accordion">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentCPU">
         <ComponentView v-bind:Parte="CurrentCPU" tipo="CPU" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentGPU">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentGPU">
         <ComponentView v-bind:Parte="CurrentGPU" tipo="GPU" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentMotherboard">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentMotherboard">
         <ComponentView v-bind:Parte="CurrentMotherboard" tipo="Motherboard" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentRAM">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentRAM">
         <ComponentView v-bind:Parte="CurrentRAM" tipo="RAM" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentCooling">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentCooling">
         <ComponentView v-bind:Parte="CurrentCooling" tipo="Cooling" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentSSD">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentSSD">
         <ComponentView v-bind:Parte="CurrentSSD" tipo="SSD" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentHDD">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentHDD">
         <ComponentView v-bind:Parte="CurrentHDD" tipo="HDD" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentPowerSupply">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentPowerSupply">
         <ComponentView v-bind:Parte="CurrentPowerSupply" tipo="PowerSupply" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentMouse">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentMouse">
         <ComponentView v-bind:Parte="CurrentMouse" tipo="Mouse" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentKeyboard">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentKeyboard">
         <ComponentView v-bind:Parte="CurrentKeyboard" tipo="Keyboard" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentMonitor">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentMonitor">
         <ComponentView v-bind:Parte="CurrentMonitor" tipo="Monitor" />
       </div>
-      <div class="p-1 col-12 col-md-6" v-if="CurrentCase">
+      <div class="p-1 col-12 col-md-8 col-lg-6 offset-lg-0 offset-md-2 offset-sm-0" v-if="CurrentCase">
         <ComponentView v-bind:Parte="CurrentCase" tipo="Case" />
       </div>
 
-      <div class="container TotalPrice">
-        <p>
+      <div class="container ">
+        <h2 class="TotalPrice">
           Total Minimo: ${{
             Total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
           }}
           COP
-        </p>
+        </h2>
       </div>
     </div>
   </div>
@@ -167,7 +167,7 @@ export default {
 @import url(https://fonts.googleapis.com/css2?family=Maven+Pro:wght@700&display=swap);
 
 .TotalPrice {
-  font-size: 4vw;
+  font-size: calc(1.8rem + 1.7vw);
   font-family: "Fredoka One", cursive;
   color: #ff9941;
   background-color: rgb(24, 23, 23);
@@ -178,7 +178,7 @@ export default {
 
 .Summary {
   font-family: "Maven Pro", sans-serif;
-  font-size: 7vw;
+  font-size: calc(1.9rem + 3.8vw);
   color: rgb(24, 23, 23);
 }
 
@@ -188,7 +188,7 @@ export default {
 }
 
 .user {
-  font-size: 4.23vw;
+  font-size: calc(1rem + 1.5vw);
   font-family: "Maven Pro", sans-serif;
   color: rgb(24, 23, 23);
 }
